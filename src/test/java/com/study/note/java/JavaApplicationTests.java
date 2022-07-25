ba2cf83d25cd9424904bbeaa5d50f8f47ca5211b
claire <summerwyd@sina.com>
2022-07-25 23:42:14 +0800

    项目初始化

---
+++ src/test/java/com/study/note/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.note.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
